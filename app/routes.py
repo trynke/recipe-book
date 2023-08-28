6a99097eace8072902ac034b76c07206bf4eb2a5
trynke <pirit544@gmail.com>
2023-08-28 21:13:51 +0500

    write basic templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, I am a recipe book!"
+    user = {'username': 'Katy'}
+    recipes = [
+        {
+            'author': {'username': 'John'},
+            'name': 'Apple pie'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'name': 'Pumpkin soup'
+        },
+        {
+            'author': {'username': 'Dora'},
+            'name': 'Pumpkin cinnamon buns'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, recipes=recipes)
