cfcb5957dd01f8f721258226d1ac8f10060943f7
trynke <pirit544@gmail.com>
2023-09-09 01:52:25 +0500

    add emails and password recovery

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from time import time
 from datetime import datetime
 from hashlib import md5
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db, login
+from app import db, login, app
 
 @login.user_loader
 def load_user(user_id):
@@ -56,6 +58,20 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Recipe.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Recipe.timestamp.desc())
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def __repr__(self):
         return f'<User {self.username}>'
